00f4f627e5ce2f194af265c50eb1dc9c2e0bba12
SofiiaKorost <104525052+SofiiaKorost@users.noreply.github.com>
2023-03-19 22:48:26 +0000

    Deploying to gh-pages from @ SofiiaKorost/goit-react-hw-08-phonebook@e596bc75d1ee5cd2deb9fdc20baa4fd2b98ab5a1 🚀

---
+++ static/js/349.382a73d9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[349],{349:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var r,a=t(168),c=t(6405).Z.h1(r||(r=(0,a.Z)(["\n  font-size: 46px;\n  display: inline-block;\n  margin-top: 60px;\n"]))),i=t(184),l=function(){return(0,i.jsx)("main",{children:(0,i.jsx)(c,{children:"Welcome page !"})})}}}]);
+//# sourceMappingURL=349.382a73d9.chunk.js.map
