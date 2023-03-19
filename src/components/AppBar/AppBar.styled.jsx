e596bc75d1ee5cd2deb9fdc20baa4fd2b98ab5a1
SofiiaKorost <korostenska@icloud.com>
2023-03-20 00:46:48 +0200

    change s

--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -6,7 +6,7 @@ export const Header = styled.header`
   align-items: center;
   justify-content: space-between;
   padding: 8px 0;
-  border-bottom: 2px solid purple;
+  border-bottom: 2px solid pink;
   > nav,
   div {
     display: flex;
